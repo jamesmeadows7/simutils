10433bc56fa98f2f33b54697810c8fd48785b8f1
jamesmeadows7 <jm2250@cantab.ac.uk>
2025-04-16 12:18:04 +0100

    Add utils for performing blocking analysis and plotting timeseries

---
+++ src/simutils/analysis/blocking.py
@@ -0,0 +1,71 @@
+import numpy as np
+import pandas as pd
+import logging
+
+logger = logging.getLogger(__name__)
+
+def blocking_analysis(series: pd.Series) -> pd.DataFrame:
+    """
+    Performs a blocking procedure on the last 2^N values of a Series, returning a DataFrame of statistics for each blocking operation.
+
+    Parameters
+    ----------
+    series : Series
+        Timeseries values.
+
+    Returns
+    -------
+    blocking_df : DataFrame
+        Number of blocks, mean, sem and sem error for each blocking operation.
+    """
+    # trim data to last 2^N data points
+    max_operations = int(np.log2(len(series)))
+    n = 2**max_operations
+    series = series.iloc[-n:]
+    # initialise results DataFrame
+    blocking_df = pd.DataFrame(index=range(max_operations), columns=["N", "mean", "sem", "sem_err"])
+    # blocking procedure
+    for o in range(max_operations):
+        # compute statistics
+        N = len(series)
+        mean = series.mean()
+        sem = series.sem()
+        sem_err = series.sem() / np.sqrt(2*(N-1))
+        blocking_df.loc[o, "N"] = N
+        blocking_df.loc[o, "mean"] = mean
+        blocking_df.loc[o, "sem"] = sem
+        blocking_df.loc[o, "sem_err"] = sem_err
+        # average adjacent values
+        series = series.groupby(np.arange(N) // 2).mean()
+    return blocking_df
+
+
+def optimal_block(blocking_df: pd.DataFrame) -> tuple[float, float]:
+    """
+    Finds the optimal block size in the blocking analysis, Calculates the mean and sem associated with this block size.
+
+    Parameters
+    ----------
+    blocking_df : DataFrame
+        Number of blocks, mean, sem and sem error for each blocking operation.
+
+    Returns
+    -------
+    mean : int
+        Mean value of the timeseries.
+    sem : int
+        Standard error in the mean value of the timeseries.
+    """
+    n = blocking_df.loc[0, "N"]
+    # compute block size
+    block_size = n / blocking_df["N"]
+    # compute optimal block size according to 10.1103/PhysRevE.83.066706
+    opt_block_size = (2*n*(blocking_df["sem"]/blocking_df["sem"].loc[0])**4)**(1/3)
+    # find smallest B that is less than B_opt
+    opt_idx = (block_size > opt_block_size).idxmax()
+    # check optimal B < n / 50
+    if block_size[opt_idx] > n / 50:
+        logger.info(f"Optimal block size is {block_size[opt_idx]}. Only {n/block_size[opt_idx]} blocks contributing to average. Gather more data for better statistics.")
+    mean = blocking_df["mean"].iloc[opt_idx]
+    sem = blocking_df["sem"].iloc[opt_idx]
+    return mean, sem
---
+++ src/simutils/plotting.py
@@ -0,0 +1,80 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.axes import Axes
+from matplotlib.figure import Figure
+
+
+def plot_timeseries(series: pd.Series, xlabel: str, ylabel: str, mean: float = None, sem: float = None, ax: Axes = None) -> tuple[Figure, Axes]:
+    """
+    Plots a timeseries showing the mean and standard error of the data if specified.
+
+    Parameters
+    ----------
+    series : Series
+        Timeseries values.
+    xlabel : str
+        Label text for the horizontal axis.
+    ylabel : str
+        Label text for the vertical axis.
+    mean : float, optional
+        Mean value of the timeseries.
+    sem : float, optional
+        Standard error in the mean value of the timeseries.
+    ax : Axis, optional
+        Existing axis to plot to.
+    
+    Returns
+    -------
+    fig : Figure
+        Plot figure.
+    ax : Axis
+        Plot axis.
+    """
+    if ax is None:
+        fig, ax = plt.subplots()
+    else:
+        fig = ax.figure
+    ax.plot(series.index, series)
+    if mean is not None:
+        ax.axhline(mean, color="tab:red", zorder=3)
+    if sem is not None:
+        x0, x1 = ax.get_xlim()
+        ax.fill_between([x0, x1], mean-sem, mean+sem, color="tab:red", alpha=0.3, zorder=3, lw=0)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.set_xlim(x0, x1)
+    return fig, ax
+
+
+def plot_blocking_analysis(blocking_df: pd.DataFrame, units: str, sem: float = None, ax: Axes = None) -> tuple[Figure, Axes]:
+    """
+    Plots a timeseries showing the mean and standard error of the data if specified.
+
+    Parameters
+    ----------
+    blocking_df : DataFrame
+        Number of blocks, mean, sem and sem error for each blocking operation.
+    units : str
+        Units of the analysed quantity.
+    sem : float, optional
+        Standard error in the mean value of the timeseries.
+    ax : Axis, optional
+        Existing axis to plot to.
+    
+    Returns
+    -------
+    fig : Figure
+        Plot figure.
+    ax : Axis
+        Plot axis.
+    """
+    if ax is None:
+        fig, ax = plt.subplots()
+    else:
+        fig = ax.figure
+    ax.errorbar(blocking_df.index, blocking_df["sem"], yerr=blocking_df["sem_err"])
+    if sem is not None:
+        ax.axhline(sem, color="tab:red", zorder=3)
+    ax.set_xlabel("Blocking Operations")
+    ax.set_ylabel(f"σ_m [{units}]")
+    return fig, ax
