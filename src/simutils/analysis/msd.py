5d1f41502538c5ab46038f74dcc12af7f3aefb81
jamesmeadows7 <jm2250@cantab.ac.uk>
2025-04-25 16:26:25 +0100

    Add serial MSD analysis

---
+++ src/simutils/analysis/msd.py
@@ -0,0 +1,89 @@
+import numpy as np
+from tqdm import tqdm
+from MDAnalysis.analysis.base import AnalysisBase
+from MDAnalysis.transformations.nojump import NoJump
+
+class MSD(AnalysisBase):
+    """
+    Mean squared displacement based on molecules CoMs.
+
+    Parameters
+    ----------
+    ag : AtomGroup
+        AtomGroup for MSD analysis.
+    msd_type : str, optional
+        Dimensions to be included in the MSD (default = xyz).
+    fft : bool, optional
+        Use FFT based algorithm (default = True).
+
+    Attributes
+    ----------
+    dim_fac : int
+        Dimensionality of the MSD.
+    results.timeseries : ndarray
+        Averaged MSD over all the particles with respect to lag-time.
+    results.msds_by_particle : ndarray
+        MSD of each individual particle with respect to lag-time.
+    """
+
+    def __init__(self, ag, msd_type="xyz", fft=True):
+        # apply NoJump transformation
+        ag.universe.trajectory.add_transformations(NoJump())
+        super(MSD, self).__init__(ag.universe.trajectory)
+        self._ag = ag
+        self._msd_type = msd_type
+        self._parse_msd_type()
+        self._fft = fft
+        self._n_residues = ag.n_residues
+
+    def _parse_msd_type(self):
+        keys = {
+            "x": [0],
+            "y": [1],
+            "z": [2],
+            "xy": [0, 1],
+            "xz": [0, 2],
+            "yz": [1, 2],
+            "xyz": [0, 1, 2],
+        }
+        self._msd_type = self._msd_type.lower()
+        try:
+            self._dim = keys[self._msd_type]
+        except KeyError:
+            raise ValueError("Invalid msd type.")
+        self._dim_fac = len(self._dim)
+
+    def _prepare(self):
+        self.results.msds_by_particle = np.zeros((self.n_frames, self._n_residues))
+        self.results._position_array = np.zeros((self.n_frames, self._n_residues, self._dim_fac))
+
+    def _single_frame(self):
+        # don't need to unwrap coms because we use NoJump
+        coms = self._ag.center_of_mass(unwrap=False, compound="residues")
+        self.results._position_array[self._frame_index] = coms[:, self._dim]
+
+    def _conclude(self):
+        if self._fft:
+            self._conclude_fft()
+        else:
+            self._conclude_simple()
+
+    def _conclude_simple(self):
+        lagtimes = np.arange(1, self.n_frames)
+        positions = self.results._position_array.astype(np.float64)
+        for lag in tqdm(lagtimes):
+            disp = positions[:-lag, :, :] - positions[lag:, :, :]
+            sqdist = np.square(disp).sum(axis=-1)
+            self.results.msds_by_particle[lag, :] = np.mean(sqdist, axis=0)
+        self.results.timeseries = self.results.msds_by_particle.mean(axis=1)
+
+    def _conclude_fft(self):
+        try:
+            import tidynamics
+        except ImportError:
+            raise ImportError("tidynamics not found")
+
+        positions = self.results._position_array.astype(np.float64)
+        for n in tqdm(range(self._n_residues)):
+            self.results.msds_by_particle[:, n] = tidynamics.msd(positions[:, n, :])
+        self.results.timeseries = self.results.msds_by_particle.mean(axis=1)
