9707ea8aa217513df82aa73592d4f79802f2feb1
jamesmeadows7 <jm2250@cantab.ac.uk>
2025-08-11 00:14:15 +0100

    Add utils for setting up lattice energy simulations

---
+++ src/simutils/setup/cgf.py
@@ -0,0 +1,15 @@
+FORCE_FIELDS = {
+    "opls":["chelpg_chelpg", "chelpg_cm1a", "ligpargen_chelpg", "ligpargen_cm1a", "opls2020_basic", "opls2020_chelpg", "opls2020_cm1a", "opls2020_cndo", "opls_basic", "opls_chelpg", "opls_cm1a", "opls_cndo"],
+    "gaff":["gaff2_abcg2", "gaff2_cndo", "gaff2_dnp", "gaff2_resp", "gaff_bcc", "gaff_cndo", "gaff_dnp", "gaff_resp"],
+}
+
+POLYMORPHS = ["alpha", "beta", "gamma"]
+
+DEFAULTS = {
+    "opls":"    1         3          yes         0.5         0.5",
+    "gaff":"    1         2          yes         0.5        0.8333"
+}
+
+BETA_ZX = {"alpha":"1e-5", "beta":"1e-5", "gamma":"0.0"}
+
+Z = {"alpha": 4, "beta": 2, "gamma": 3}
--- src/simutils/setup/utils.py
+++ src/simutils/setup/utils.py
@@ -1,5 +1,6 @@
 from os import PathLike
 from string import Template
+import subprocess
 
 
 def write_from_template(template: PathLike, output: PathLike, substitutions: dict) -> None:
@@ -19,3 +20,25 @@ def write_from_template(template: PathLike, output: PathLike, substitutions: dic
         result = Template(f.read()).substitute(substitutions)
     with open(output, "w") as f:
         f.write(result)
+
+
+def replicate_unitcell(unitcell: PathLike, supercell: PathLike, n_replicas: list) -> None:
+    """
+    Replaces $-identifiers in template file with values in substitution dictionary and writes to output file.
+
+    Parameters
+    __________
+    unitcell : PathLike
+        Path of the gro file containing the unitcell.
+    supercell : PathLike
+        Path of the created supercell.
+    n_replicas : list
+        Number of replicas in each dimension [nx, ny, nz].
+    """
+    nx, ny, nz = n_replicas
+    p = subprocess.run(
+    f"gmx genconf -f {unitcell} -nbox {nx} {ny} {nz} -o {supercell}",
+    shell=True,
+    check=True,
+    capture_output=True,
+    )
