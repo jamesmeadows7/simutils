2d9332a3f357a985e2c0cefcd87baea26210a817
jamesmeadows7 <jm2250@cantab.ac.uk>
2025-08-13 14:15:03 +0100

    Add function to configure logging

--- src/simutils/__init__.py
+++ src/simutils/__init__.py
@@ -1,3 +1,15 @@
 from pint import UnitRegistry
+import logging
 
 ureg = UnitRegistry()
+
+def set_logging_config():
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format="[%(levelname)s %(asctime)s] %(name)s: %(message)s",
+        datefmt='%m-%d %H:%M:%S',
+    )
+
+    logging.getLogger("MDAnalysis").setLevel(logging.WARNING)
+    logging.getLogger("pymbar").setLevel(logging.WARNING)
